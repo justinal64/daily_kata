4decea3275fed255820205e53d14ec7518e56770
justin leggett <justin@codingrookie.com>
2017-09-19 15:54:28 -0500

    completed 'can decode test'

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -1,30 +1,42 @@
 const Cipher = function(key) {
-  randomValue = () => {
-    return Math.floor(Math.random() * 25) + 97; // random number between a - z
-  };
+  // randomValue = () => {
+  //   return Math.floor(Math.random() * 25) + 97; // random number between a - z
+  // };
 
   this.key = "a";
+  // https://stackoverflow.com/questions/17572873/how-can-i-check-if-a-string-is-all-uppercase-in-javascript
+  isUpperCase = stringToCheck => {
+    return /^[^A-Z]*$/.test(stringToCheck);
+  };
 
   // checks the string to determine if all chars are the same
   compareString = stringToCompare => {
+    if (isUpperCase(stringToCompare)) {
+      new Error("Bad key");
+    }
     for (var i = 0; i < stringToCompare.length; i++) {
+      // if all the values aren't the same return false
       if (stringToCompare[i] !== stringToCompare[0]) return false;
     }
     return true;
   };
 
   scrambleText = charToEncode => {
-    var scrambledValue = this.key.charCodeAt() + charToEncode.charCodeAt();
-    if (scrambledValue > 122) return scrambledValue - 97;
-    return scrambledValue;
+    const scrambledValue = this.key.charCodeAt() + charToEncode.charCodeAt();
+    if (scrambledValue > 122) return String.fromCharCode(scrambledValue - 97);
+    return String.fromCharCode(scrambledValue);
   };
 
   this.encode = encodedString => {
     if (compareString(encodedString)) {
-      var scrambledValue = scrambleText(encodedString[0]);
-      return String.fromCharCode(scrambledValue);
+      return scrambleText(encodedString[0]);
     }
-    return "b";
+    // throw an error
+    return encodedString;
+  };
+
+  this.decode = stringToEncode => {
+    console.log(stringToEncode);
   };
 };
 
